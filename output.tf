81aacaeb29c71239a9e00376334aa82a0cd69fa2
aadi308 <yaditya308@gmail.com>
2022-06-21 15:59:00 +0545

    changes

--- output.tf
+++ output.tf
@@ -1,15 +1,3 @@
 output "vpc-west" {
   value = module.vpc-west.vpc_id
 }
-
-output "vpc-east" {
-  value = module.vpc-east.vpc_id
-}
-
-output "vpc_peering_connection" {
-  value = aws_vpc_peering_connection.pc.id
-}
-
-
-
-

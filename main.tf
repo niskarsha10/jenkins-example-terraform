4ea48ef0236cd155d57d77ebb99399c999ad9c42
aadi308 <yaditya308@gmail.com>
2022-06-21 17:38:28 +0545

    final

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_security_group_rule" "example" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["10.10.0.0/16"]
+  description       = "test"
   security_group_id = aws.aws_Security_group.id
 }
